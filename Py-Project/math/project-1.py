7d40d4ace5c1c5fc868f8d03f4eba6a8f4b6ac7e
kapil <24f2007712@ds.study.iitm.ac.in>
2026-02-08 21:46:53 +0530

    sum of Two numbers

---
+++ Py-Project/math/project-1.py
@@ -0,0 +1,9 @@
+# adding 2 Numbers 
+
+num_1 = int(input("Enter First Number:- "))
+num_2 = int(input("Enter Second Number:- "))
+
+result = num_1 + num_2
+
+print(f'sum of {num_1} and {num_2} is {result}')
+

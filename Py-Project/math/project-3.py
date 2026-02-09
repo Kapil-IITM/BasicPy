97f08270b3ed5f83319c83da403b8e4d20ad9cfe
kapil <24f2007712@ds.study.iitm.ac.in>
2026-02-09 22:19:10 +0530

    Celsius to Fahrenheit

---
+++ Py-Project/math/project-3.py
@@ -0,0 +1,7 @@
+# convert Celsius to Fahrenheit
+
+Temprature_C = float(input("Enter Temprature in C :- "))
+
+Fahrenheit = (Temprature_C * 9/5 ) + 32
+
+print(f"{Temprature_C} C is equal to {Fahrenheit} F")

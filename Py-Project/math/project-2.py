a403cb3a9f6db27767f2d83aaf2541dea794a854
kapil <24f2007712@ds.study.iitm.ac.in>
2026-02-09 16:23:45 +0530

    area of triangle

---
+++ Py-Project/math/project-2.py
@@ -0,0 +1,8 @@
+# Find area of triangle
+
+base = int(input("Enter base of triangle in cm :- "))
+height = int(input("Enter height of triangle in cm :- "))
+
+area_of_triangle = 1/2*base*height
+
+print(f"area of triangle is {area_of_triangle} cm^2")

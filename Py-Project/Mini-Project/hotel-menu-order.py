5c3c151f48596a2e14bb4b20851a5aa3f4256064
kapil <24f2007712@ds.study.iitm.ac.in>
2026-02-25 20:48:09 +0530

    Menu update

---
+++ Py-Project/Mini-Project/hotel-menu-order.py
@@ -0,0 +1,23 @@
+# Hotel Menu >>>>
+
+menu = {
+    "Pizza":299,
+    "Veg-Burger":99,
+    "Non-Veg Burger":199,
+    "Maggie":99,
+    "Pasta":99,
+    "Cold Drink" : 69,
+    "Tea":50,
+    "Hot Coffee": 69,
+    "Cold Coffee":99
+
+}
+
+#Greet to coustomer 
+name = input("Please enter your name :- ")
+print(f"Welcome {name} to Hotel XYZ ")
+
+# Hotel Menu 
+
+print("Pizza - Rs. 299/-\nVeg-Burger - Rs. 99/-\nNon-Veg Burger - Rs. 199/- \nMaggie - Rs. 99/-\nPasta Rs. 99/- \nCold Drink - Rs.69/- \nTea - Rs.50/- \nHot Coffee: Rs. 69/- \nCold Coffee - Rs. 99")
+

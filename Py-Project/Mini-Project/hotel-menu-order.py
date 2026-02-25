9b7ca6201aa06b9a7023a1a8839d3d638a7beb45
kapil <24f2007712@ds.study.iitm.ac.in>
2026-02-25 21:05:47 +0530

    Hotel menu order

--- Py-Project/Mini-Project/hotel-menu-order.py
+++ Py-Project/Mini-Project/hotel-menu-order.py
@@ -21,3 +21,25 @@ print(f"Welcome {name} to Hotel XYZ ")
 
 print("Pizza - Rs. 299/-\nVeg-Burger - Rs. 99/-\nNon-Veg Burger - Rs. 199/- \nMaggie - Rs. 99/-\nPasta Rs. 99/- \nCold Drink - Rs.69/- \nTea - Rs.50/- \nHot Coffee: Rs. 69/- \nCold Coffee - Rs. 99")
 
+order_total = 0 # This Variable take price of orderd food 
+
+
+first_item = input("Enter the item you want to order :- ")
+
+if first_item in menu:
+    order_total += menu[first_item]
+    print(f"your item {first_item} has been added to order")
+else:
+    print(f"your ordered item {first_item} is not available in menu please order something else that we can serve you")
+
+another_order = input("Do you want to add another item (Yes/No):-")
+
+if another_order == "Yes":
+    second_item = input("Enter the second item you want to order :- ")
+    if second_item in menu:
+        order_total += menu[second_item]
+        print(f"your item {second_item} has been added to order")
+    else:
+        print(f"your ordered item {second_item} is not available in menu please order something else that we can serve you")
+
+print(f"The total amount of items to pay is {order_total}")
